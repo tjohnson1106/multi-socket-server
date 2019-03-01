8ab5d5e685ce0ec275707b799410f952f0cca354
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-01 17:49:50 -0500

    created handle in new chat function in room channel

--- lib/chat_server_web/channels/room_channel.ex
+++ lib/chat_server_web/channels/room_channel.ex
@@ -2,6 +2,14 @@ defmodule ChatServerWeb.RoomChannel do
   use ChatServer, :channel
 
   def join("rooms:" <> room_id, _payload, socket) do
-    {:ok, assign(socket, room_id, String.to_integer(room_id))}
+    {:ok, assign(socket, :room_id, String.to_integer(room_id))}
+  end
+
+  def handle_in("new_chat", payload, socket) do
+    broadcast!(socket, "new_chat", %{
+      body: payload["body"]
+    })
+
+    {:reply, :ok, socket}
   end
 end
